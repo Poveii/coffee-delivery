f3e6c1b49aa4eacf996401602c14780e87109574
Poveii <yellowtutors@gmail.com>
2023-10-27 17:52:46 -0300

    feat: Default Theme and Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-  return <div>Coffee Delivery</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div>Coffee Delivery</div>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
