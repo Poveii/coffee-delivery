b8636ee5b29cdc6b643775537c123fcee9e1e717
Poveii <yellowtutors@gmail.com>
2024-01-23 17:37:39 -0300

    fix: Unnecessary ESLint disable rule comment in `styled.d.ts`

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -3,6 +3,5 @@ import { defaultTheme } from '../styles/themes/default'
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends ThemeType {}
+  export type DefaultTheme = ThemeType
 }
